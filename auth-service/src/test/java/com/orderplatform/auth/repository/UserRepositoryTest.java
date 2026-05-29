e28172952cb70b5fb55eb7440e40643e7a336fbd
Nik_SSD <nik_rink@mail.ru>
2026-05-29 20:05:39 +0300

    Поправил тесты для CI

--- auth-service/src/test/java/com/orderplatform/auth/repository/UserRepositoryTest.java
+++ auth-service/src/test/java/com/orderplatform/auth/repository/UserRepositoryTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Optional;
 import java.util.Set;
@@ -15,6 +16,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
 class UserRepositoryTest {
 
 
