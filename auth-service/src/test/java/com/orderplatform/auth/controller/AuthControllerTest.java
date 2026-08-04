c429bebdf39d4e0450f5ae879cd6391ccdcfaca3
Nik_SSD <nik_rink@mail.ru>
2026-08-04 12:27:47 +0300

    Исправлен AuthControllerTest.java

--- auth-service/src/test/java/com/orderplatform/auth/controller/AuthControllerTest.java
+++ auth-service/src/test/java/com/orderplatform/auth/controller/AuthControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
@@ -23,10 +24,13 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 class AuthControllerTest {
+
+    @LocalServerPort
+    private int port;
 
     @Autowired
     private MockMvc mockMvc;
